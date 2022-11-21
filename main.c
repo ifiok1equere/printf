d1877e14514eb5dc338f1912d55d941a0b28b1f2
6godpro <erickson5115@gmail.com>
2022-11-21 15:45:56 -0800

    fixed buffer output

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main(void)
 	int len;
 
 	len = _printf("Let's try to printf a simple sentence.\n");
-	_printf("%d", len);
+	_printf("%d, %c\n", len, 'A');
 	_printf("Length:[%d, %i]\n", len, len);
 	_printf("Negative:[%d]\n", 762534);
 	return (0);
